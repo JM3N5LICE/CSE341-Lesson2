f341fc4fb2cb0265addb7243441011e2da84516c
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-01-11 21:30:47 -0700

    initial commit (assignment 1)

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const awesomeFunction = (req, res, next) => {
+    res.json('Ted Lasso');
+};
+
+const aBryantsBBQSauce = (req, res, next) => {
+    res.json('I love Ted Lasso');
+};
+
+module.exports = { awesomeFunction, aBryantsBBQSauce};
